b34236880f721fecaec9d0efa22b90511e497d47
Zub04ek <nastasyz@ukr.net>
2023-03-05 16:27:52 +0200

    add 2nd completed task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from "react";
+import { Toaster } from 'react-hot-toast';
+import { Container } from "./Container.styled";
+import { GlobalStyles } from "./Global.styled";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { Searchbar } from "./Searchbar/Searchbar";
+
+export class App extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  handleSubmit = searchQuery => {
+    this.setState({ searchQuery });
+  }
+
+  render() {
+    return (
+    <Container>
+        <GlobalStyles />
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+        />
+        <Searchbar onSubmit={this.handleSubmit} />
+        <ImageGallery searchQuery={this.state.searchQuery} />
+    </Container>
   );
+  }
 };
